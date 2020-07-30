a40a971171bdbff586687abb98ac164d0dac6583
narinn-star <skfls2618@naver.com>
2020-07-30 23:49:03 +0900

    animation effect_menu

--- example3/js/example3.js
+++ example3/js/example3.js
@@ -1,17 +1,49 @@
 var Btn1 = document.querySelector(".b1")
-var menuform = document.querySelector(".menuform")
+var Btn2 = document.querySelector(".b2")
+var Btn3 = document.querySelector(".b3")
+var menuform1 = document.querySelector(".menuform1")
+var menuform2 = document.querySelector(".menuform2")
+var menuform3 = document.querySelector(".menuform3")
 var overlay = document.querySelector(".overlay")
-var exit = document.querySelector(".exit")
+var exit1 = document.querySelector(".exit1")
+var exit2 = document.querySelector(".exit2")
+var exit3 = document.querySelector(".exit3")
 
 function HandlerBtn1(){
-    menuform.style.left = "0px"
+    menuform1.style.left = "0px"
     overlay.classList.remove("hidden")
 }
 
-function HandlerExit(){
-    menuform.style.left = "-500px"
+function HandlerExit1(){
+    menuform1.style.left = "-500px"
     overlay.classList.add("hidden")
 }
 
 Btn1.addEventListener("click",HandlerBtn1)
-exit.addEventListener("click",HandlerExit)
+exit1.addEventListener("click",HandlerExit1)
+
+function HandlerBtn2(){
+    menuform2.style.left = "0px"
+    overlay.classList.remove("hidden")
+}
+
+function HandlerExit2(){
+    menuform2.style.left = "-500px"
+    overlay.classList.add("hidden")
+}
+
+Btn2.addEventListener("click", HandlerBtn2)
+exit2.addEventListener("click",HandlerExit2)
+
+function HandlerBtn3(){
+    menuform3.style.left = "0px"
+    overlay.classList.remove("hidden")
+}
+
+function HandlerExit3(){
+    menuform3.style.left = "-500px"
+    overlay.classList.add("hidden")
+}
+
+Btn3.addEventListener("click", HandlerBtn3)
+exit3.addEventListener("click",HandlerExit3)
